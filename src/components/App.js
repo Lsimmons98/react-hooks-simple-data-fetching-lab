f1171462b6b705c98939973dbff54bf5ae301fa3
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-17 11:38:21 -0400

    Fetch dog pictures

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,17 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+const App = () => {
+const [dogPicture, setDogPicture] = useState('')
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => r.json())
+      .then((r) => setDogPicture(r.message))
+  }, []);
+
+ return dogPicture ? <img src={dogPicture} alt='A Random Dog'/> : <p>Loading</p>
+
+
+}
+
+export default App;
